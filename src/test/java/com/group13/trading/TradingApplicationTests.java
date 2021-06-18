577334bdff314825dcb7446462bb3b8e25b3e13d
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2021-06-18 10:40:56 +0200

    Initial commit

---
+++ src/test/java/com/group13/trading/TradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group13.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
